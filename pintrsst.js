cd7af8d2851c73b712771d13303e3c5af0a41736
Alexander Mahan <alexmahan@gmail.com>
2013-01-23 16:32:09 -0800

    fixing tabs/spaces

--- pintrsst.js
+++ pintrsst.js
@@ -1,35 +1,38 @@
 (function($){
+	$.fn.extend({
+		pintRSSt: function (options) {
+		
+			var settings = {
+				boardRSS: 'http://pinterest.com/tacobell/doritos-locos-tacos.rss',
+				items: 5,
+				container: 'li'
+			}
 
-	$.fn.extend({  
-	 
-    pintRSSt: function(options) {
-      var defaults = {
-        boardRSS: 'http://pinterest.com/tacobell/doritos-locos-tacos.rss',
-        items: 5,
-        container: 'li'
-      }
-           
-      var options =  $.extend(defaults, options);
+			var options =  $.extend(settings, options);
 
-      return this.each(function() {
-        var o = options;
-        		obj = $(this);
-        		wrapper = o.container;
-			  $.getJSON('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&callback=?&q='+encodeURIComponent(o.boardRSS)+'', function(data){
-			    $.each(data.responseData.feed.entries, function(i, item){
-				    var description = item.content;
-				    $(obj).append('<'+wrapper+' class="pinterest-post">'+description+'</'+wrapper+'>');
-				    return i<o.items-1;
-			    }); 
-		    	$(''+wrapper+'.pinterest-post a').each(function(){
-			    	var src = $(this).attr('href');
-				    $(this).attr('href', 'http://pinterest.com'+src);
+			return this.each(function () {
+			
+				var o = options;
+					obj = $(this);
+					wrapper = o.container;
+					
+				$.getJSON('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&callback=?&q='+encodeURIComponent(o.boardRSS)+'', function(data) {
+				
+					$.each(data.responseData.feed.entries, function (i, item) {
+						var description = item.content;
+						$(obj).append('<'+wrapper+' class="pinterest-post">'+description+'</'+wrapper+'>');
+						return i<o.items-1;
+					}); 
+					
+					$(''+wrapper+'.pinterest-post a').each(function(){
+						var src = $(this).attr('href');
+						$(this).attr('href', 'http://pinterest.com'+src);
 					});
-				});   
-      });
-      
-    }
-    
-  });
-  
+					
+				});
+				
+			});
+			
+		}
+	});
 })(jQuery);
